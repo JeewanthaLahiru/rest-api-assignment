d436059658c2ecd8ee2c95bcf462b1369c653158
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-11-25 19:47:51 +0530

    gson

--- app/src/main/java/com/example/rest_api_fundamental/SecondFragment.kt
+++ app/src/main/java/com/example/rest_api_fundamental/SecondFragment.kt
@@ -1,12 +1,27 @@
 package com.example.rest_api_fundamental
 
+import android.content.IntentFilter.create
+import android.net.DnsResolver
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.renderscript.ScriptIntrinsicBLAS.create
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.rest_api_fundamental.posts.api.PostApi
+import com.example.rest_api_fundamental.posts.model.Post
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory.create
+import java.net.URI.create
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -18,6 +33,29 @@ class SecondFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        val gson = GsonBuilder()
+            .create()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+        var getPost = retrofit.create(PostApi::class.java)
+        var postCall = getPost.post
+        postCall.enqueue(object : Callback<Post>{
+            override fun onFailure(call: Call<Post>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                var title = response.body()!!.title
+                Log.d("secondFragment",title)
+            }
+
+        })
+
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
--- app/src/main/java/com/example/rest_api_fundamental/posts/api/PostApi.java
+++ app/src/main/java/com/example/rest_api_fundamental/posts/api/PostApi.java
@@ -1,4 +1,12 @@
 package com.example.rest_api_fundamental.posts.api;
 
+import com.example.rest_api_fundamental.posts.model.Post;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface PostApi {
+
+    @GET("/posts/1")
+    Call<Post> getPost();
 }
